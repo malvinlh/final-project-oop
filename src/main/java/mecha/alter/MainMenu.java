6904e88b785e1bfaee10e5b86e9bd23971d062be
MalvinLH <malvinleo21@gmail.com>
2023-11-27 13:53:13 +0700

    Add Custom Fonts

--- src/main/java/mecha/alter/MainMenu.java
+++ src/main/java/mecha/alter/MainMenu.java
@@ -2,6 +2,7 @@ package mecha.alter;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.FontFormatException;
 import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -10,11 +11,14 @@ import java.awt.event.MouseListener;
 import java.awt.geom.RoundRectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.io.InputStream;
+
 import javax.imageio.ImageIO;
 
 public class MainMenu implements MouseListener
 {
     private GamePanel gp;
+    Font imprisha, maruMonica;
     private BufferedImage backgroundImage;
     public RoundRectangle2D.Double storyModeButton, optionButton, encyclopediaButton, creditsButton;
     private int buttonSpacing = 40;
@@ -31,8 +35,16 @@ public class MainMenu implements MouseListener
         // Load the background image
         try 
         {
+        	InputStream imp = getClass().getResourceAsStream("/fonts/IMPRISHA.TTF");
+        	InputStream maruMon = getClass().getResourceAsStream("/fonts/x12y16pxMaruMonica.ttf");
+        	imprisha = Font.createFont(Font.TRUETYPE_FONT, imp);
+        	maruMonica = Font.createFont(Font.TRUETYPE_FONT, maruMon);
             backgroundImage = ImageIO.read(getClass().getResourceAsStream("/mainmenu/bg2.png"));
         } 
+        catch (FontFormatException e)
+        {
+        	e.printStackTrace();
+        }
         catch (IOException e) 
         {
             e.printStackTrace();
@@ -54,55 +66,53 @@ public class MainMenu implements MouseListener
             g.fillRect(0, 0, gp.getWidth(), gp.getHeight());
         }
 
-        Font fontTitle = new Font("Serif", Font.BOLD, 100);
-        g.setFont(fontTitle);
+        g.setFont(imprisha.deriveFont(Font.BOLD, 100));
         g.setColor(Color.WHITE);
 
-        FontMetrics fontMetrics = g.getFontMetrics(fontTitle);
-        int titleWidth = fontMetrics.stringWidth("ALTER");
-        int titleHeight = fontMetrics.getHeight();
+        int titleWidth = g.getFontMetrics().stringWidth("ALTER");
+        int titleHeight = g.getFontMetrics().getHeight();
 
         int xTitle = (gp.getWidth() - titleWidth) / 2;
-        int yTitle = (gp.getHeight() - titleHeight) / 8 + fontMetrics.getAscent(); // Adjusted the division factor
+        int yTitle = (gp.getHeight() - titleHeight) / 8 + g.getFontMetrics().getAscent(); // Adjusted the division factor
 
         g.drawString("ALTER", xTitle, yTitle);
         
         g2d.setColor(Color.WHITE);
         
-        Font fontStory = new Font("Serif", Font.BOLD, 45);
+        Font fontStory = new Font("Arial", Font.BOLD, 45);
         g.setFont(fontStory);
         FontMetrics fontMetricsStory = g.getFontMetrics(fontStory);
         int storyModeTextWidth = fontMetricsStory.stringWidth("Story Mode");
         int storyModeTextHeight = fontMetricsStory.getHeight();
-        g2d.drawString("Story Mode", (int) (xTitle + (storyModeButton.width - storyModeTextWidth) / 2), (int) (yTitle + 30 + titleHeight + buttonSpacing - storyModeTextHeight / 2));
-        storyModeButton.setFrame(xTitle, yTitle + titleHeight + buttonSpacing - 50, storyModeButton.width, storyModeButton.height);
+        g2d.drawString("Story Mode", (int) (xTitle + 20 + (storyModeButton.width - storyModeTextWidth) / 2), (int) (yTitle + 30 + titleHeight + buttonSpacing - storyModeTextHeight / 2));
+        storyModeButton.setFrame(xTitle + 20, yTitle + titleHeight + buttonSpacing - 50, storyModeButton.width, storyModeButton.height);
         g2d.draw(storyModeButton);
         
-        Font fontOption = new Font("Serif", Font.BOLD, 45);
+        Font fontOption = new Font("Arial", Font.BOLD, 45);
         g.setFont(fontOption);
         FontMetrics fontMetricsOption = g.getFontMetrics(fontOption);
         int optionTextWidth = fontMetricsOption.stringWidth("Option");
         int optionTextHeight = fontMetricsOption.getHeight();
-        g2d.drawString("Option", (int) (xTitle - 3 + (optionButton.width - optionTextWidth) / 2), (int) (storyModeButton.y + 19 + storyModeButton.height + buttonSpacing + optionTextHeight / 2));
-        optionButton.setFrame(xTitle, storyModeButton.y + storyModeButton.height + buttonSpacing, optionButton.width, optionButton.height);
+        g2d.drawString("Options", (int) (xTitle + 8 + (optionButton.width - optionTextWidth) / 2), (int) (storyModeButton.y + 24 + storyModeButton.height + buttonSpacing + optionTextHeight / 2));
+        optionButton.setFrame(xTitle + 20, storyModeButton.y + storyModeButton.height + buttonSpacing, optionButton.width, optionButton.height);
         g2d.draw(optionButton);
 
-        Font fontEncyclopedia = new Font("Serif", Font.BOLD, 45);
+        Font fontEncyclopedia = new Font("Arial", Font.BOLD, 45);
         g.setFont(fontEncyclopedia);
         FontMetrics fontMetricsEncyclopedia = g.getFontMetrics(fontEncyclopedia);
         int encyclopediaTextWidth = fontMetricsEncyclopedia.stringWidth("Encyclopedia");
         int encyclopediaTextHeight = fontMetricsEncyclopedia.getHeight();
-        g2d.drawString("Encyclopedia", (int) (xTitle + 2 + (encyclopediaButton.width - encyclopediaTextWidth) / 2), (int) (optionButton.y + 20 + optionButton.height + buttonSpacing + encyclopediaTextHeight / 2));
-        encyclopediaButton.setFrame(xTitle, optionButton.y + optionButton.height + buttonSpacing, encyclopediaButton.width, encyclopediaButton.height);
+        g2d.drawString("Encyclopedia", (int) (xTitle + 19 + (encyclopediaButton.width - encyclopediaTextWidth) / 2), (int) (optionButton.y + 25 + optionButton.height + buttonSpacing + encyclopediaTextHeight / 2));
+        encyclopediaButton.setFrame(xTitle + 20, optionButton.y + optionButton.height + buttonSpacing, encyclopediaButton.width, encyclopediaButton.height);
         g2d.draw(encyclopediaButton);
 
-        Font fontCredits = new Font("Serif", Font.BOLD, 45);
+        Font fontCredits = new Font("Arial", Font.BOLD, 45);
         g.setFont(fontCredits);
         FontMetrics fontMetricsCredits = g.getFontMetrics(fontCredits);
         int creditsTextWidth = fontMetricsCredits.stringWidth("Credits");
         int creditsTextHeight = fontMetricsCredits.getHeight();
-        g2d.drawString("Credits", (int) (xTitle + (creditsButton.width - creditsTextWidth) / 2), (int) (encyclopediaButton.y + 20 + encyclopediaButton.height + buttonSpacing + creditsTextHeight / 2));
-        creditsButton.setFrame(xTitle, encyclopediaButton.y + encyclopediaButton.height + buttonSpacing, creditsButton.width, creditsButton.height);
+        g2d.drawString("Credits", (int) (xTitle + 19 + (creditsButton.width - creditsTextWidth) / 2), (int) (encyclopediaButton.y + 25 + encyclopediaButton.height + buttonSpacing + creditsTextHeight / 2));
+        creditsButton.setFrame(xTitle + 20, encyclopediaButton.y + encyclopediaButton.height + buttonSpacing, creditsButton.width, creditsButton.height);
         g2d.draw(creditsButton);
     }
 
